e3d9ecda52f968de651e64259bc214cd9b906a3a
derickcastillo38 <9derick@gmail.com>
2018-11-06 10:31:05 -0500

    updated database to have more spread out data for future filtering and search functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,177 +1,175 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-derick = User.create(username: 'D-Rock', password: 'D-Rock')
-isiah = User.create(username: 'Crow', password: 'Crow')
-nkosi = User.create(username: 'Cos', password: 'Cos')
+alfred = User.create(username: 'alfred', password: '123')
+andrew = User.create(username: 'andrew', password: '123')
+alex = User.create(username: 'aaron', password: '123')
 
-king_concert = derick.host_events.create(
+king_concert = alfred.host_events.create(
   event_name: 'Emily King Concert',
   event_description: 'Live acoustic set by Emily King',
-  host_name: 'Derick',
-  host_description: 'I think Emily King has a great voice and is a great performer. Im excited to expose her artistry to others',
+  host_name: 'Alfred',
+  host_description: "I think Emily King has a great voice and is a great performer. I'm excited to expose her artistry to others",
   date: 'Thu Oct 11 2018',
-  location: 'Access Labs Rootop',
-  whats_included: 'Drinks and Rooftop Access',
+  location: 'Belurg Restaurant and Bar',
+  whats_included: 'Drinks and Private Room',
   time_length: '2 hours',
-  group_size: 2,
-  price: 20,
+  group_size: 10,
+  price: 30,
   image_url: "https://image.ibb.co/iBzx6L/Screen-Shot-2018-10-15-at-6-50-43-PM.png")
 
 
-xx_concert = isiah.host_events.create(
-  event_name: 'XX Concert',
-  event_description: 'Live set by XX',
-  host_name: 'Isiah',
-  host_description: 'I think XX puts out awesome music and Im excited to expose their artistry to others',
+double_x_concert = andrew.host_events.create(
+  event_name: 'Double X Concert',
+  event_description: 'Live set by Double X',
+  host_name: 'Andrew',
+  host_description: "I think Double X is a great band and I'm excited to let others enjoy their music",
   date: 'Fri Oct 12 2018',
-  location: 'Access Labs Rootop',
-  whats_included: 'Drinks and Rooftop Access',
+  location: 'Brinton Lounge',
+  whats_included: 'Drinks and Exclusive Access',
   time_length: '2 hours',
-  group_size: 3,
-  price: 15,
+  group_size: 12,
+  price: 30,
   image_url: "https://image.ibb.co/co7jmL/Screen-Shot-2018-10-15-at-6-50-57-PM.png")
 
 
-blake_concert = nkosi.host_events.create(
+blake_concert = alex.host_events.create(
   event_name: 'James Blake Concert',
   event_description: 'Live set by James Blake',
-  host_name: 'Nkosi',
-  host_description: 'I think James Blake puts out awesome music and Im excited to expose their artistry to others',
+  host_name: 'Alex',
+  host_description: 'I am a close friend of James Blake and know that others will enjoy his voice',
   date: 'Sat Oct 13 2018',
-  location: 'Access Labs Rootop',
-  whats_included: 'Drinks and Rooftop Access',
+  location: 'Crinkerton Lounge',
+  whats_included: 'Drinks and Exclusive Access',
   time_length: '2 hours',
-  group_size: 3,
-  price: 30,
+  group_size: 10,
+  price: 35,
   image_url: "https://image.ibb.co/kQhdD0/Screen-Shot-2018-10-15-at-6-51-13-PM.png")
-cooking_class = nkosi.host_events.create(
+cooking_class = alfred.host_events.create(
   event_name: 'Italian Cooking Class',
-  event_description: 'Make 5 signature Italian dishes with a real Italian',
-  host_name: 'Nkosi',
-  host_description: 'Im a real Italian and a great cook',
+  event_description: 'Make 5 signature Italian dishes with a professional cook',
+  host_name: 'Alfred',
+  host_description: "I've been cooking all my life and will show you how to cook great dishes",
   date: 'Sat Oct 13 2018',
-  location: 'Brooklyn',
+  location: 'Giovann Restaurant and Lounge',
   whats_included: 'Ingredients, Wine',
   time_length: '3 hours',
-  group_size: 10,
-  price: 30,
+  group_size: 9,
+  price: 25,
   image_url: "https://image.ibb.co/erfDgL/cooking.png")
-pottery_class = nkosi.host_events.create(
+pottery_class = andrew.host_events.create(
   event_name: 'Pottery Lessons',
   event_description: 'We will make 2 great pieces of pottery together',
-  host_name: 'Nkosi',
-  host_description: 'I have made pottery since I was a baby',
+  host_name: 'Andrew',
+  host_description: 'I have been making pottery since I was a child and I love teaching others',
   date: 'Sat Oct 13 2018',
-  location: 'Queens',
+  location: 'Pottery and Things',
   whats_included: 'Tools, Wine',
-  time_length: '2 hours',
-  group_size: 3,
-  price: 30,
+  time_length: '2.5 hours',
+  group_size: 7,
+  price: 15,
   image_url: "https://image.ibb.co/c4aFT0/pottery.png")
-dj_lesson = nkosi.host_events.create(
+dj_lesson = alex.host_events.create(
   event_name: 'Professional DJ Lesson',
-  event_description: 'I will teach you how to spin records and make tracks.',
-  host_name: 'Nkosi',
-  host_description: 'Im a professional DJ',
+  event_description: 'I will teach you how to spin records, make tracks, and you will leave with a recorded CD.',
+  host_name: 'Alex',
+  host_description: 'Im a professional DJ and have worked with Sony for 20 years',
   date: 'Sat Oct 13 2018',
-  location: 'Manhattan',
+  location: 'Manhattan loft in Soho',
   whats_included: 'Turntables, Recorded CD',
   time_length: '2 hours',
   group_size: 3,
-  price: 40,
+  price: 45,
   image_url: "https://image.ibb.co/cgktFf/deejay.png")
-comedy_show = nkosi.host_events.create(
+comedy_show = alfred.host_events.create(
   event_name: 'Private Comedy Show',
   event_description: 'Intimate comedy show on a rooftop deck',
-  host_name: 'Nkosi',
+  host_name: 'Alfred',
   host_description: 'Been making people laugh since 1986',
   date: 'Sat Oct 13 2018',
-  location: 'Brooklyn Rootop',
+  location: 'Brooklyn Loft Rootop',
   whats_included: 'Drinks and Rooftop Access',
   time_length: '2 hours',
-  group_size: 3,
-  price: 10,
+  group_size: 12,
+  price: 25,
   image_url: "https://image.ibb.co/iPJKo0/comedy.png")
-brooklyn_bar_hop = nkosi.host_events.create(
+brooklyn_bar_hop = andrew.host_events.create(
   event_name: 'Brooklyn Bar Hop',
   event_description: 'Grab drinks at Brooklyns finest',
   host_name: 'Nkosi',
   host_description: 'I know all the best bars',
   date: 'Sat Oct 13 2018',
-  location: 'Brooklyn',
-  whats_included: 'tour, exclusive access',
-  time_length: '2 hours',
-  group_size: 3,
-  price: 30,
+  location: 'BK Tavern on the Ridge',
+  whats_included: 'Tour, Exclusive Access',
+  time_length: '3 hours',
+  group_size: 15,
+  price: 25,
   image_url: "https://image.ibb.co/i76N80/barhop.png")
-wine_n_painting = nkosi.host_events.create(
+wine_n_painting = alex.host_events.create(
   event_name: 'Wine and Painting',
   event_description: 'Painting lessons while drinking great wine',
-  host_name: 'Nkosi',
+  host_name: 'Alex',
   host_description: 'I love wine and painting and so will you',
   date: 'Sat Oct 13 2018',
-  location: 'Bronx',
-  whats_included: 'Materials, red wine',
-  time_length: '2 hours',
-  group_size: 3,
+  location: 'Fronto Studio',
+  whats_included: 'Materials, Red Wine',
+  time_length: '3 hours',
+  group_size: 9,
   price: 20,
   image_url: "https://image.ibb.co/kKjW1L/painting.png")
-cigar_making = nkosi.host_events.create(
+cigar_making = alfred.host_events.create(
   event_name: 'Cigar Making Lessons',
   event_description: 'We will make cigars and then smoke them',
-  host_name: 'Nkosi',
+  host_name: 'Alfred',
   host_description: 'My family owns a cigar business and I love it',
   date: 'Sat Oct 13 2018',
-  location: 'Brooklyn',
-  whats_included: 'Materials, lounge access',
+  location: 'Pure Smokes Lounge',
+  whats_included: 'Materials, Lounge Access',
   time_length: '2 hours',
   group_size: 3,
   price: 30,
   image_url: "https://image.ibb.co/cNWuML/cigar.png")
-salsa_lessons = nkosi.host_events.create(
+salsa_lessons = andrew.host_events.create(
   event_name: 'Professional Salsa Lessons',
   event_description: 'We will learn to salsa, then dance the night away',
-  host_name: 'Nkosi',
+  host_name: 'Andrew',
   host_description: 'I have danced since a child and even danced with Shakira twice!',
   date: 'Sat Oct 13 2018',
-  location: 'Brooklyn',
-  whats_included: 'Lessons, dance studio access',
+  location: 'Dance Fever Studio',
+  whats_included: 'Lessons, Wine',
   time_length: '3 hours',
-  group_size: 3,
+  group_size: 15,
   price: 30,
   image_url: "https://image.ibb.co/hKWpo0/salsa.png")
-graffiti_tour = nkosi.host_events.create(
+graffiti_tour = alex.host_events.create(
   event_name: 'Hidden Graffiti Tour',
   event_description: 'Navigate Brooklyn streets to view fantastic graffiti',
-  host_name: 'Nkosi',
-  host_description: 'I know where all the best graffiti is because I tagged it!',
+  host_name: 'Alex',
+  host_description: 'I know where all the best graffiti is because I tagged most of it!',
   date: 'Sat Oct 13 2018',
-  location: 'Brooklyn Streets',
-  whats_included: 'Exclusive tour, photos',
-  time_length: '2 hours',
-  group_size: 3,
-  price: 30,
+  location: 'Various Brooklyn Streets',
+  whats_included: 'Exclusive Tour, Photos',
+  time_length: '3 hours',
+  group_size: 10,
+  price: 15,
   image_url: "https://image.ibb.co/jLBYFf/graffiti.png")
-pizza_making = nkosi.host_events.create(
+pizza_making = alfred.host_events.create(
   event_name: 'Pizza Making Lessons',
   event_description: 'We will make handcrafted artisan pizza',
-  host_name: 'Nkosi',
-  host_description: 'I have made pizza since I was 5',
+  host_name: 'Alfred',
+  host_description: 'I have been making pizza since I was 5. I also own my own restaurant',
   date: 'Sat Oct 13 2018',
-  location: 'Staten Island',
-  whats_included: 'Ingredients, photos',
-  time_length: '2 hours',
-  group_size: 3,
-  price: 30,
+  location: 'Marinati Pizza Restaurant',
+  whats_included: 'Ingredients, Photos',
+  time_length: '3 hours',
+  group_size: 10,
+  price: 20,
   image_url: "https://image.ibb.co/nQvM1L/pizza.png")
 
-king_concert.guests << nkosi
-xx_concert.guests << derick
-xx_concert.guests << nkosi
-blake_concert.guests << derick
-blake_concert.guests << isiah
+pottery_class.guests << alfred
+salsa_lessons.guests << alfred
+graffiti_tour.guests << alfred
+
+comedy_show.guests << andrew
+pizza_making.guests << andrew
+
+brooklyn_bar_hop.guests << alex
+king_concert.guests << alex
+cooking_class.guests << alex
